156b63374db3d71b846615d65a52e28908c9b54b
pramod ade <prmdextracthere@gmail.com>
2023-12-22 00:36:26 +0530

    after writing logic

--- src/components/Expense_Item.jsx
+++ src/components/Expense_Item.jsx
@@ -1,13 +1,20 @@
 
 import "./Expense_Item.css"
-function ExpenseItem () {
+
+
+function ExpenseItem (props) {
+  console.log(props);
+
+  // const expenseDate = new Date(2021,2,28);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = 294.67;
   return (
     <div className="expense-item">
       <div className="expense-item__description">
-        March 28th 2021</div>
+        {props.date.toString()}</div>
       <div className="expense-item__price">
-        <h2>Car Insurance</h2>
-        <div>$294.67</div>
+        <h2>{props.title}</h2>
+        <div>${props.amount}</div>
       </div>
     </div>
   )
