edc41c7629e272652c9bfdcd4d9d8ab846904e84
pramod ade <prmdextracthere@gmail.com>
2024-01-03 00:44:13 +0530

    assignment 4

--- src/AddExpense/ExpenseForm.js
+++ src/AddExpense/ExpenseForm.js
@@ -1,26 +1,52 @@
-import React from "react";
+import { useState } from 'react';
+import React  from "react";
 import './ExpenseForm.css'
 const ExpenseForm = () =>{
-  const titleChangeHandler = (event) =>{
-    console.log(event.target.value);
+  const [enterTitle,setEnterTittle]= useState('');
+  const [enterAmount,setEnterAmount]=useState('');
+  const [enterDate,setEnterDate]=useState('');
+
+  
+
+  const TitleChangeHandler = (event) =>{
+    setEnterTittle(event.target.value);
   }
-  return ( <form>
+
+  const amountChangeHandler = (event) =>{
+    setEnterAmount(event.target.value);
+  }
+
+  const dateChangeHandler = (event) =>{
+    setEnterDate(event.target.value);
+  }
+
+  const submitHandler = (event) =>{
+    event.preventDefault();
+    const expenseData ={
+      title:enterTitle,
+      amount:enterAmount,
+      date:new Date(enterDate)
+    };
+    console.log(expenseData);
+  }
+
+  return ( <form onSubmit={submitHandler}>
     <div className="new-expense__controls">
       <div className="new-expense__control">
         <label>Title</label>
-        <input type="text" onChange={titleChangeHandler}/>
+        <input type="text" onChange={TitleChangeHandler} />
       </div>
       <div className="new-expense__control">
         <label>Amount</label>
-        <input type="number" min="0.01" step="0.01"/>
+        <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
       </div>
       <div className="new-expense__control">
         <label>Date</label>
-        <input type="date" min="2019-01-01" max="2023-12-31"/>
+        <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChangeHandler} />
       </div>
     </div>
     <div className="new-expense__actions">
-      <button type="submit">Add Expense</button>
+      <button type="submit" >Add Expense</button>
     </div>
   </form>
 
--- src/Feature/Expense_Item.jsx
+++ src/Feature/Expense_Item.jsx
@@ -5,17 +5,12 @@ import Card from "../UI/Card_Element";
 
 const ExpenseItem = (props) => {
   const [title,setTitle] = useState(props.title);
-  const [expense,setExpense] = useState(props.amount);
   
   const clikedHandler = () => {
    setTitle("update");
    console.log(title);
   };
 
-  const clikedHandler_expense = () => {
-    setExpense("$100");
-    console.log(expense);
-  }
 
   return (
     <Card className="expense-item">
@@ -23,8 +18,7 @@ const ExpenseItem = (props) => {
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div 
-        className="expense-item__price">{expense}
-        <button onClick={clikedHandler_expense}>update expense</button>
+        className="expense-item__price">${props.amount}
         </div>
         <button onClick={clikedHandler}>change Title</button>
       </div>
