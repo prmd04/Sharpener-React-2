c7a6a7a91025bca58c4fbd7cbc3c936113191a3c
pramod ade <prmdextracthere@gmail.com>
2024-01-05 13:03:31 +0530

    display on the screen

--- src/App.js
+++ src/App.js
@@ -2,36 +2,39 @@ import Expenses from "./Feature/Expense";
 import React,{useState} from "react";
 import NewExpense from "./AddExpense/newExpense";
 
-const expenses = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-  },
-];
+
 
 
 function App() {
-  const [addNewEntry,setAddNewEntry] = useState(expenses);
-  const addNewExpenseHandler = (expense) =>{
-    console.log("in appjs");
-    expenses.push(expense);
-    setAddNewEntry(expenses);
-  }
+  const initialExpenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  const addNewExpenseHandler = (newExpense) => {
+  setExpenses((prevExpenses) => {
+    return [...prevExpenses, newExpense];
+  });
+};
+
 
   return (
     <div>
